d8aeeff19580725df7323e1ba6bbe3bdee5259d8
Soundarya10 <sreddy.0720@gmail.com>
2018-04-06 12:36:14 -0400

    Payroll Information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecentActivityComponent } from './recent-activity/recent-activity.component';
+import { PayrollInformationComponent } from './payroll-information/payroll-information.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecentActivityComponent
+    RecentActivityComponent,
+    PayrollInformationComponent
   ],
   imports: [
     BrowserModule
